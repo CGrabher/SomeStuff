ae3e11bd2a2820f5c19a12dcd520026436a8c607
CGrabher <119925918+CGrabher@users.noreply.github.com>
2023-07-09 19:23:43 +0200

    Change class of Random Fill

--- TicTacToeLibary/Database/DatabaseFacade.cs
+++ TicTacToeLibary/Database/DatabaseFacade.cs
@@ -79,4 +79,34 @@ public class DatabaseFacade
         context.SaveChanges();
     }
 
+
+    /// <summary>
+    /// adds 10 random players with 25-50 random games
+    /// </summary>
+    public void fillDatabase()
+    {
+        var scores = new int[] { 0, 5, 10 };
+        var random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+
+            var newPlayer = new HumanPlayer("Player" + i);
+
+            var numberOfGames = random.Next(25, 50);
+            for (int j = 0; j < numberOfGames; j++)
+            {
+                var gr = new GameResult();
+                gr.Score = scores[random.Next(3)];
+
+                // guess random time in last 3 months (~ 6.5 mio seconds)
+                gr.DateTime = DateTime.Now.Subtract(new TimeSpan(0, 0, random.Next(5000, 6_500_000)));
+                newPlayer.GameResults.Add(gr);
+                gr.Player = newPlayer;
+            }
+
+            context.Add(newPlayer);
+        }
+        context.SaveChanges();
+    }
+
 }
--- TicTacToeLibary/Database/TicTacToeDatabaseContext.cs
+++ TicTacToeLibary/Database/TicTacToeDatabaseContext.cs
@@ -29,35 +29,5 @@ namespace TicTacToeLibary.Database
             options.UseSqlite($"Data Source={DbPath}");
             Console.WriteLine($"Data Source={DbPath}");
         }
-
-        /// <summary>
-        /// adds 10 random players with 25-50 random games
-        /// </summary>
-        public void fillDatabase()
-        {
-            var scores = new int[]{ 0, 5, 10};
-            var random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-
-                var newPlayer = new HumanPlayer("Player" + i);
-
-                var numberOfGames = random.Next(25, 50); 
-                for (int j = 0; j < numberOfGames; j++) {
-                    var gr = new GameResult();
-                    gr.Score = scores[random.Next(3)];
-
-                    // guess random time in last 3 months (~ 6.5 mio seconds)
-                    gr.DateTime = DateTime.Now.Subtract(new TimeSpan(0, 0, random.Next(5000, 6_500_000)));
-                    newPlayer.GameResults.Add(gr);
-                    gr.Player = newPlayer;
-                }
-
-                this.Add(newPlayer);
-            }
-            this.SaveChanges();
-        }
-
-
     }
 }
